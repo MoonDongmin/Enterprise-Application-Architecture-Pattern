11a70a0d5db3abea1462fee367d04c77cc3c21fc
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-10-29 21:53:05 +0900

    13장. 객체-관계형 메타데이터 매핑 패턴

--- Practice/class-table-inheritance/src/mapper/mapper.ts
+++ Practice/class-table-inheritance/src/mapper/mapper.ts
@@ -9,23 +9,22 @@ export class Mapper {
 
     // 검색 메서드
     protected findAll() {
-        throw new Error("findAll() 메서드는 서브클래스에서 구현해야 합니다.");
+        throw new Error("findAll() 메서드는 하위클래스에서 구현해야 합니다.");
     }
 
 
     // 삽입 메서드
     protected async insert(obj: DomainObject): Promise<void> {
-        throw new Error("insert() 메서드는 서브클래스에서 구현해야 합니다.");
+        throw new Error("insert() 메서드는 하위클래스에서 구현해야 합니다.");
     }
 
     // 업데이트 메서드
     protected async update(obj: DomainObject): Promise<void> {
-        throw new Error("update() 메서드는 서브클래스에서 구현해야 합니다.");
+        throw new Error("update() 메서드는 하위클래스에서 구현해야 합니다.");
     }
 
     // 데이터 삭제 메서드
     public async delete(obj: DomainObject): Promise<void> {
-        throw new Error("update() 메서드는 서브클래스에서 구현해야 합니다.");
+        throw new Error("update() 메서드는 하위클래스에서 구현해야 합니다.");
     }
-
 }
--- README.md
+++ README.md
@@ -33,6 +33,8 @@ height="40px"/></a>
 
 [Chapter 12장. 객체-관계형 구조 패턴](https://moondongmin.notion.site/Chapter-12-11e357c44e5680dab0e1c86cc952a1b5?pvs=4)
 
+[13장. 객체-관계형 메타데이터 매핑 패턴](https://moondongmin.notion.site/13-12d357c44e568004b3c8ea31fc379cc4?pvs=4)
+
 [18장. 기본 패턴](https://moondongmin.notion.site/18-d515be9cb2934f06a1ca7a9b625892e5?pvs=4)
 
 ## "UML 실전에서는 이것만 쓴다"
